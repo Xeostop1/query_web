543ecad1a932953f5aeca1d9018c7ca16a805311
Xeostop1 <stop1@daum.net>
2024-02-29 13:42:20 +0900

    add: ele

--- Python/Geumseongchimdae_auto.py
+++ Python/Geumseongchimdae_auto.py
@@ -8,8 +8,22 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 import subprocess
 import time
+# 바로 40 view로 넘어가서 시작
+
+# 이름
+# item_detail_tit h3
+# 이거 클릭plus_review_num_tit
+# 3번째 li
+# strong
+# 뒤로가기 누르고 
+
+
+
+
+
 
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
@@ -20,52 +34,32 @@ options.add_argument(f"user-agent={user_agent}")
 driver = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=options)
 
+
+wait = WebDriverWait(driver, 10)
 # 웹사이트 접속
 driver.get('https://www.ksbed.co.kr/goods/goods_list.php?cateCd=002&sort=&pageNum=40#')
 
+gallery = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'item_gallery_type')))
+print(f'gallery 찾기 성공')
 
-login_button = driver.find_element(By.ID, 'idLoginBtn')  
-login_button.click()
-print(f'login_button 클릭 로그인')
+links = gallery.find_elements(By.CSS_SELECTOR, 'ul li .item_photo_box a')
+print(f'links 찾기 성공')
+for i in range(len(links)):
+    # 매번 새롭게 요소찾기(페이지 변경때문에)
+    links = gallery.find_elements(By.CSS_SELECTOR, 'ul li .item_photo_box a')
+    # i번째 요소를 클릭합니다.
+    links[i].click()  
 
-try:
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'iptBtnEm')))
-    pass_btn = driver.find_element(By.ID, 'iptBtnEm')
-    pass_btn.click()
-    print(f'비밀번호 다음에 변경')
+    pcs_name = driver.find_element(By.CSS_SELECTOR, '.item_detail_tit h3').text
+    print("상품명: ", pcs_name)
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, '백업받기/올리기')))
-    backup_link = driver.find_element(By.LINK_TEXT, '백업받기/올리기')
-    backup_link.click()
-    print(f'{backup_link} 클릭')
+    hits = driver.find_element(By.CSS_SELECTOR, '.plus_review_num_tit ul li:last-child strong').text
 
-    radio_btn=driver.find_element(By.NAME,'mssqlDbIdx')
-    radio_btn.click()
-    call_DB=driver.find_element(By.ID,'btnBackup')
-    call_DB.click()
-    print(f'{call_DB} 성공')
-except NoSuchElementException:
-    print("iptBtnEm 요소가 없습니다. 백업받기/올리기로 이동합니다.")
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, '백업받기/올리기')))
-    backup_link = driver.find_element(By.LINK_TEXT, '백업받기/올리기')
-    backup_link.click()
-    print(f'{backup_link} 클릭')
 
-    radio_btn=driver.find_element(By.NAME,'mssqlDbIdx')
-    radio_btn.click()
-    call_DB=driver.find_element(By.ID,'btnBackup')
-    call_DB.click()
-    print(f'{call_DB} 성공')
+    print("조회수: ", hits)
 
-# pass_btn = driver.find_element(By.ID, 'iptBtnEm')  
-# pass_btn.click()
-# print(f'{pass_btn} 다음에 변경')
+    # 원래 페이지 돌아오기
+    driver.back()  
+    print("원래 페이지 돌기")
 
-print(f'20분 대기')
-time.sleep(1000)
-#========================================
-#===============FTP exe 실행=================
-#========================================
-
-print(f'파일질러 실행')
-subprocess.Popen(["C:\\Program Files\\FileZilla FTP Client\\filezilla.exe"])
+print("완료")
